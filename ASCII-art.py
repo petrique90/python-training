8b373ddb928a23cf3817cd52f61022fe678d067f
Petrik Saari <saaripetrik@gmail.com>
2021-03-18 11:13:46 +0200

    Added ASCII art python training

---
+++ ASCII-art.py
@@ -0,0 +1,23 @@
+#Petrik Saari
+#Fun fact: I make my own beer!
+
+print("PPPP   SSS ")
+print("P   P S   S")
+print("P   P S    ")
+print("PPPP   SSS ")
+print("P         S")
+print("P     S   S")
+print("P      SSS ")
+
+beer = """
+            &&&
+  &&&&&&&& &&&&&
+ &&&&&&&&&&&&&&&&
+ &|&&&|   |   | |===#}
+ &|&& |   |   | |   B}
+ &|&  |   |   | |   B}
+ &|   |   |   | |   B}
+  |   |   |   | |===#}
+  |___|___|___|_|
+"""
+print(beer)
